c5e5909441fd4724ed7829d7714d321dc346554d
monba08 <32450677+monba08@users.noreply.github.com>
2018-03-15 09:59:52 +0100

    First Commit

--- src/Accounts.java
+++ src/Accounts.java
@@ -1,16 +1,30 @@
 import java.lang.*;
+import java.util.Scanner;
 
 
 public class Accounts {
 
-    private String CoinType;
+    private String coinType;
     private String nameUser;
+    private int valueCoin;
     private int quantityCoin;
     private int date;
 
 
     public void createNewAccount(){
-
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = reader.next();
+        System.out.println("Enter a password for your account: ");
+        String password = reader.next();
+        System.out.println("Which crypto-coin would you like to add? ");
+        String coin = reader.next();
+        System.out.println("Type in the current value of this coin: ");
+        int value = reader.nextInt();
+        nameUser = name;
+        coinType=coin;
+        valueCoin=value;
+        reader.close();
     }
 
     public void logIn(String Name){
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
+    //private Accounts Account;
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Welcome to CryptoLogger!");
+        Accounts account = new Accounts();
+        account.createNewAccount();
+
     }
+
+
 }
