d65f18e7002af6ce5f99b1ad8f3b19a56142664a
Satish Singh <raju-52@hotmail.com>
2018-04-07 12:06:21 +0200

    Ey man heb beetje aan GUI gewerkt, run het is en check het eens. De code erachter moeten we combineren met de bestaande anders doen we dubbel werk.

--- src/cryptoGui.java
+++ src/cryptoGui.java
@@ -32,7 +32,7 @@ public class cryptoGui {
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible(true); //r
+        frame.setVisible(true);
     }
 
     public cryptoGui() {
